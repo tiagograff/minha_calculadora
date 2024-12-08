d021ec3e6a1a98d0492eaa5b95929b53d47b6cf2
tiago <tiagopinzgraffunder@gmail.com>
2024-12-08 14:04:34 -0300

    feat: colocando funcionalidades no display

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,35 @@
 const mode = document.querySelector(".nav__image");
-const root = document.documentElement; // Seleciona o :root
+const root = document.documentElement;
+const charButton = document.querySelectorAll(".charKey");
+let display = document.getElementById("displayCalculatorInput");
+
+function blockBoard(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+document.addEventListener("keydown", blockBoard);
+document.addEventListener("keypress", blockBoard);
+document.addEventListener("keyup", blockBoard);
+
+charButton.forEach((key) => {
+  key.addEventListener("click", () => {
+    if (key.classList.contains("operator")) {
+      const value = key.dataset.value + " ";
+      display.value += value;
+    } else if (
+      key.classList.contains("number") ||
+      key.classList.contains("float")
+    ) {
+      const value = key.dataset.value;
+      display.value += value;
+    } else if (key.classList.contains("equal")) {
+      console.log("aqui vai ser calculado");
+    } else if (key.classList.contains("clear")) {
+      display.value = "";
+    }
+  });
+});
 
 mode.addEventListener("click", () => {
   if (root.classList.contains("lightmode")) {
